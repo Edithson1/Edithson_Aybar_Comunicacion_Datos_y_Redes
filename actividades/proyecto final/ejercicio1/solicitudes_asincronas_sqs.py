984d6cc96a5bc84b7037c5bc819cad3ccf40a551
Edithson Aybar <152218004+Edithson1@users.noreply.github.com>
2024-07-02 23:25:25 -0500

    Update solicitudes_asincronas_sqs.py

--- actividades/proyecto final/ejercicio1/solicitudes_asincronas_sqs.py
+++ actividades/proyecto final/ejercicio1/solicitudes_asincronas_sqs.py
@@ -9,6 +9,7 @@ def crear_cola_sqs(nombre):
         response = sqs.create_queue(
             QueueName=nombre
         )
+        print(f"Cola {nombre} creada exitosamente")
         return response['QueueUrl']
     except ClientError as e:
         print(f"Error al crear la cola: {e}")
@@ -47,38 +48,35 @@ def leer_y_procesar_mensajes(queue_url):
     try:
         # Crear un cliente de SQS
         sqs = boto3.client('sqs')
-        while True:
-            # Recibir mensajes de la cola SQS
-            response = sqs.receive_message(
-                QueueUrl=queue_url,
-                MaxNumberOfMessages=10,
-                WaitTimeSeconds=20
-            )
-            mensajes = response.get('Messages', [])
-            if not mensajes:
-                print("No hay mensajes en la cola.")
-                continue
-            for mensaje in mensajes:
-                print(f"Mensaje recibido: {mensaje['Body']}")
-                # Procesar el mensaje enviándolo a S3
-                procesar_enviar_a_s3(mensaje)
+        # Recibir mensajes de la cola SQS
+        response = sqs.receive_message(
+            QueueUrl=queue_url,
+            MaxNumberOfMessages=10,
+            WaitTimeSeconds=20
+        )
+        mensajes = response.get('Messages', [])
+        for mensaje in mensajes:
+            print(f"Mensaje recibido: {mensaje['Body']}")
+            # Procesar el mensaje enviándolo a S3
+            procesar_enviar_a_s3(mensaje)
 
     except ClientError as e:
         print(f"Error al leer o procesar mensajes: {e}")
 
-if __name__ == "__main__":
-    nombre_cola = 'MiColaSQS'
+#Ejemplo de ejecucion
 
-    # Crear la cola SQS
-    queue_url = crear_cola_sqs(nombre_cola)
+nombre_cola = 'MiColaSQS'
+
+# Crear la cola SQS
+queue_url = crear_cola_sqs(nombre_cola)
     
-    # Enviar un mensaje a la cola SQS
-    if queue_url:
-        mensajes = [
-            'Mensaje 1 para la cola SQS',
-            'Mensaje 2 para la cola SQS',
-            'Mensaje 3 para la cola SQS'
-        ]
-        enviar_mensajes_a_cola(queue_url, mensajes)
-    if queue_url:
-        leer_y_procesar_mensajes(queue_url)
+# Enviar un mensaje a la cola SQS
+if queue_url:
+    mensajes = [
+        'Mensaje 1 para la cola SQS',
+        'Mensaje 2 para la cola SQS',
+        'Mensaje 3 para la cola SQS'
+    ]
+    enviar_mensajes_a_cola(queue_url, mensajes)
+
+    leer_y_procesar_mensajes(queue_url)
